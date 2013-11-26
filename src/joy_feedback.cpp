e8d28e85bdd8a659e1602c9946be67be067159a5
lucasw <wsacul@gmail.com>
2013-11-26 06:22:08 -0800

    Can build this but it isn't tested, it should be possible to setup a rumble effect on index 0 then play it by sending a 0 to the play topic.

--- src/joy_feedback.cpp
+++ src/joy_feedback.cpp
@@ -2,11 +2,17 @@
 //
 
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/input.h>
 
-#include "ros/ros.h"
-#include "std_msgs/Float32.h"
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/UInt16.h>
+
+#include "joy_feedback_ros/Envelope.h"
+#include "joy_feedback_ros/Rumble.h"
+#include "joy_feedback_ros/Periodic.h"
 
 class JoyFeedback {
 public:
@@ -14,11 +20,13 @@ public:
   JoyFeedback();
   
 private:
-  void freqCallback(const std_msgs::Float32 & msg);
+  void rumbleCallback(const joy_feedback_ros::Rumble::ConstPtr& msg);
+  void playCallback(const std_msgs::UInt16::ConstPtr& msg);
   bool init();
   ros::NodeHandle nh_;
 
-  ros::Subscriber sub_;
+  ros::Subscriber rumble_sub_;
+  ros::Subscriber play_sub_;
 
   std::string device_;
   int fd_;
@@ -27,15 +35,52 @@ private:
   bool initted_;
 };
 
-void JoyFeedback::freqCallback(const std_msgs::Float32 & msg)
+void JoyFeedback::playCallback(const std_msgs::UInt16::ConstPtr& msg)
+{
+  const int ind = msg->data;
+  if (ind >= effects_.size()) {
+    ROS_WARN_STREAM("play index is too big " << ind << " " << effects_.size()
+      << " " << strerror(errno)); 
+  }
+
+  struct input_event play;
+  play.type = EV_FF;
+  play.code = effects_[ind].id;
+  // has this effect been uploaded yet?
+  if (play.code < 0) return;
+  // set this to zero to stop the effect
+  play.value = 1;
+
+  if (write(fd_, (const void*) &play, sizeof(play)) == -1) {
+    ROS_WARN_STREAM("play failed " << ind << " " << strerror(errno)); 
+  }
+}
+
+
+
+void JoyFeedback::rumbleCallback(const joy_feedback_ros::Rumble::ConstPtr& msg)
 {
   //ros
   if (!initted_) return;
+
+  const int ind = 0;
+  effects_[ind].type = FF_RUMBLE;
+  effects_[ind].id = -1;
+  effects_[ind].u.rumble.strong_magnitude = msg->strong_magnitude;
+  effects_[ind].u.rumble.weak_magnitude   = msg->weak_magnitude;
+  effects_[ind].replay.length = 5000;
+  effects_[ind].replay.delay = 1000;
+
+  if (ioctl(fd_, EVIOCSFF, &effects_[ind]) < 0) {
+    ROS_WARN_STREAM("ioctl upload effect " << ind << " " << strerror(errno));
+  }
 }
 
-JoyFeedback::JoyFeedback()
+JoyFeedback::JoyFeedback() : 
+  fd_(0)
 {
-  sub_ = nh_.subscribe("frequency", 1, freqCallback, this);
+  rumble_sub_ = nh_.subscribe<joy_feedback_ros::Rumble>("rumble", 1, &JoyFeedback::rumbleCallback, this);
+  play_sub_ = nh_.subscribe("play", 1, &JoyFeedback::playCallback, this);
   initted_ = init();
 }
 
@@ -59,6 +104,9 @@ bool JoyFeedback::init()
     ROS_WARN_STREAM("ioctl max num effects query " << strerror(errno));
   }
   ROS_INFO_STREAM("number of ff effects " << max_effects);
+  
+  if (max_effects < 1) return false;
+
   effects_.resize(max_effects);
 
   return true;
