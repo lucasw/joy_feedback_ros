52b6af8e28ae06a0f68a084ef956e219b3cae48b
lucasw <wsacul@gmail.com>
2015-12-08 03:49:38 -0800

    roslint compliance with http://lucasw.github.io/roslint_1/

--- src/joy_feedback.cpp
+++ src/joy_feedback.cpp
@@ -14,11 +14,12 @@
 #include "joy_feedback_ros/Rumble.h"
 #include "joy_feedback_ros/Periodic.h"
 
-class JoyFeedback {
+class JoyFeedback
+{
 public:
 
   JoyFeedback();
-  
+
 private:
   void rumbleCallback(const joy_feedback_ros::Rumble::ConstPtr& msg);
   void periodicCallback(const joy_feedback_ros::Periodic::ConstPtr& msg);
@@ -33,7 +34,7 @@ private:
   std::string device_;
   int fd_;
   unsigned long features_[4];
-  std::vector<struct ff_effect> effects_; 
+  std::vector<struct ff_effect> effects_;
   bool initted_;
 };
 
@@ -41,9 +42,10 @@ void JoyFeedback::playCallback(const std_msgs::UInt16::ConstPtr& msg)
 {
   const int ind = msg->data;
   ROS_INFO_STREAM("play " << ind);
-  if (ind >= effects_.size()) {
+  if (ind >= effects_.size())
+  {
     ROS_WARN_STREAM("play index is too big " << ind << " " << effects_.size()
-      << " " << strerror(errno)); 
+                    << " " << strerror(errno));
   }
 
   struct input_event play;
@@ -54,15 +56,16 @@ void JoyFeedback::playCallback(const std_msgs::UInt16::ConstPtr& msg)
   // set this to zero to stop the effect
   play.value = 1;
 
-  if (write(fd_, (const void*) &play, sizeof(play)) == -1) {
-    ROS_WARN_STREAM("play failed " << ind << " " << strerror(errno)); 
+  if (write(fd_, (const void*) &play, sizeof(play)) == -1)
+  {
+    ROS_WARN_STREAM("play failed " << ind << " " << strerror(errno));
   }
 }
 
 void JoyFeedback::rumbleCallback(const joy_feedback_ros::Rumble::ConstPtr& msg)
 {
   if (!initted_) return;
-  
+
   ROS_INFO_STREAM("rumble set " << msg->strong_magnitude << " " << msg->weak_magnitude);
 
   const int ind = 0;
@@ -74,7 +77,8 @@ void JoyFeedback::rumbleCallback(const joy_feedback_ros::Rumble::ConstPtr& msg)
   effects_[ind].replay.length = 4000; // 4 seconds
   effects_[ind].replay.delay = 100;
 
-  if (ioctl(fd_, EVIOCSFF, &effects_[ind]) < 0) {
+  if (ioctl(fd_, EVIOCSFF, &effects_[ind]) < 0)
+  {
     ROS_WARN_STREAM("ioctl upload effect " << ind << " " << strerror(errno));
   }
 }
@@ -82,14 +86,14 @@ void JoyFeedback::rumbleCallback(const joy_feedback_ros::Rumble::ConstPtr& msg)
 void JoyFeedback::periodicCallback(const joy_feedback_ros::Periodic::ConstPtr& msg)
 {
   if (!initted_) return;
-  
-  ROS_INFO_STREAM("periodic set " << int(msg->waveform) << " " << msg->period 
-      << " " << msg->magnitude);
+
+  ROS_INFO_STREAM("periodic set " << int(msg->waveform) << " " << msg->period
+                  << " " << msg->magnitude);
 
   const int ind = 0;
   effects_[ind].type = FF_PERIODIC;
   effects_[ind].id = -1;
-  
+
   if (msg->waveform == joy_feedback_ros::Periodic::WAVEFORM_FF_SQUARE)
     effects_[ind].u.periodic.waveform = FF_SQUARE;
   else if (msg->waveform == joy_feedback_ros::Periodic::WAVEFORM_FF_TRIANGLE)
@@ -100,7 +104,7 @@ void JoyFeedback::periodicCallback(const joy_feedback_ros::Periodic::ConstPtr& m
     effects_[ind].u.periodic.waveform = FF_SAW_UP;
   else if (msg->waveform == joy_feedback_ros::Periodic::WAVEFORM_FF_SAW_DOWN)
     effects_[ind].u.periodic.waveform = FF_SAW_DOWN;
-  
+
   effects_[ind].u.periodic.period = msg->period;
   effects_[ind].u.periodic.magnitude = msg->magnitude;
   effects_[ind].u.periodic.offset = msg->offset;
@@ -109,18 +113,19 @@ void JoyFeedback::periodicCallback(const joy_feedback_ros::Periodic::ConstPtr& m
   effects_[ind].u.periodic.envelope.attack_level = msg->envelope.attack_level;
   effects_[ind].u.periodic.envelope.fade_length = msg->envelope.fade_length;
   effects_[ind].u.periodic.envelope.fade_level = msg->envelope.fade_level;
-  effects_[ind].trigger.button = 0x0; 
-  effects_[ind].trigger.interval = 0; 
-  effects_[ind].direction = 0x4000; 
+  effects_[ind].trigger.button = 0x0;
+  effects_[ind].trigger.interval = 0;
+  effects_[ind].direction = 0x4000;
   effects_[ind].replay.length = 4000;
   effects_[ind].replay.delay = 100;
 
-  if (ioctl(fd_, EVIOCSFF, &effects_[ind]) < 0) {
+  if (ioctl(fd_, EVIOCSFF, &effects_[ind]) < 0)
+  {
     ROS_WARN_STREAM("ioctl upload effect " << ind << " " << strerror(errno));
   }
 }
 
-JoyFeedback::JoyFeedback() : 
+JoyFeedback::JoyFeedback() :
   fd_(0)
 {
   rumble_sub_ = nh_.subscribe<joy_feedback_ros::Rumble>("rumble", 1, &JoyFeedback::rumbleCallback, this);
@@ -136,22 +141,25 @@ bool JoyFeedback::init()
   ROS_INFO_STREAM("device " << device_);
 
   fd_ = open(device_.c_str(), O_RDWR);
-  if (fd_ == -1) {
+  if (fd_ == -1)
+  {
     ROS_WARN_STREAM("Open device file " << strerror(errno));
     return false;
   }
 
-  if (ioctl(fd_, EVIOCGBIT(EV_FF, sizeof(unsigned long) * 4), features_) < 0) {
+  if (ioctl(fd_, EVIOCGBIT(EV_FF, sizeof(unsigned long) * 4), features_) < 0)
+  {
     ROS_WARN_STREAM("ioctl feature query " << strerror(errno));
     return false;
   }
 
   int max_effects;
-  if (ioctl(fd_, EVIOCGEFFECTS, &max_effects) < 0) {
+  if (ioctl(fd_, EVIOCGEFFECTS, &max_effects) < 0)
+  {
     ROS_WARN_STREAM("ioctl max num effects query " << strerror(errno));
   }
   ROS_INFO_STREAM("number of ff effects " << max_effects);
-  
+
   if (max_effects < 1) return false;
 
   effects_.resize(max_effects);
